5b443671db24a4e1f1ac932b9f19ae55c64adfba
Siddhart <Siddhart_7@live.nl>
2021-08-15 21:24:30 +0200

    vercel couldnt ready the .env

--- generateChunks.js
+++ generateChunks.js
@@ -3,7 +3,8 @@ require("dotenv").config();
 
 //supabase variables
 const { createClient }  =  require("@supabase/supabase-js");
-const SupabaseURL = require("./next.config").env.SUPABASEURL;
+// const SupabaseURL = require("./next.config").env.SUPABASEURL;
+const SupabaseURL = "https://apbrajlcunciizanpygs.supabase.co";
 const SupabaseService = process.env.SERVICE;
 
 const fs = require("fs");
