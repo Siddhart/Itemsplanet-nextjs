ee3438f7757a99794af76a5d7b8cd9ae578b9101
Siddhart <Siddhart_7@live.nl>
2021-08-17 23:30:26 +0200

    never mind will try again tomorrow

--- components/Ads.js
+++ components/Ads.js
@@ -1,20 +1,7 @@
 const Ads = () => {
   return (
     <>
-      <script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7427232256160357"
-        crossorigin="anonymous"
-      ></script>
-      <ins
-        className="adsbygoogle"
-        style={{display:"block"}}
-        data-ad-client="ca-pub-7427232256160357"
-        data-ad-slot="8299116360"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-      ></ins>
-      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+        
     </>
   );
 };
