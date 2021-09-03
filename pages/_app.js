1a824c594a314c544ba96bf350083543338b7094
siddhart <siddhart_7@live.nl>
2021-09-03 19:04:42 +0200

    lets try the favicon

--- pages/_app.js
+++ pages/_app.js
@@ -12,6 +12,7 @@ import { useCookies } from "react-cookie";
 
 //next components
 import Router, { useRouter } from "next/router";
+import Head from 'next/head'
 
 //css files
 import "../styles/cleanup.min.css";
@@ -180,6 +181,17 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      <Head>
+        <link rel="apple-touch-icon" sizes="72x72" href="./favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="./favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="./favicon/favicon-16x16.png" />
+        <link rel="manifest" href="./favicon/site.webmanifest" />
+        <link rel="mask-icon" href="./favicon/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="shortcut icon" href="./favicon/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#603cba" />
+        <meta name="msapplication-config" content="./favicon/browserconfig.xml" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <Component {...pageProps} saveToUser={saveToUser} />
       {cookieAccept == false ? (
         <CookiePopup cookieFunction={acceptCookies} />
