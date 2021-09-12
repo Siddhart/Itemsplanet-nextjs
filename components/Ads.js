402e24063f9585ad9ee4f702d8f4c8eeb9895710
Siddhart <Siddhart_7@live.nl>
2021-09-12 14:05:44 +0200

    remove g ad

--- components/Ads.js
+++ components/Ads.js
@@ -2,23 +2,24 @@ import React, { useEffect } from "react";
 // import Head from "next/head";
 
 const Ads = () => {
-  useEffect(() => {
-    try {
-      if (window) {
-        (window.adsbygoogle = window.adsbygoogle || []).push({});
-      }
-    }catch{}
-  }, []);
+  // useEffect(() => {
+  //   try {
+  //     if (window) {
+  //       (window.adsbygoogle = window.adsbygoogle || []).push({});
+  //     }
+  //   }catch{}
+  // }, []);
 
   return (
-    <ins
-      className="adsbygoogle"
-      style={{ margin: "25px auto 25px auto", display: "block" }}
-      data-ad-client="ca-pub-7427232256160357"
-      data-ad-slot="8734531951"
-      data-ad-format="auto"
-      data-full-width-responsive="true"
-    ></ins>
+    // <ins
+    //   className="adsbygoogle"
+    //   style={{ margin: "25px auto 25px auto", display: "block" }}
+    //   data-ad-client="ca-pub-7427232256160357"
+    //   data-ad-slot="8734531951"
+    //   data-ad-format="auto"
+    //   data-full-width-responsive="true"
+    // ></ins>
+    <></>
   );
 };
 
