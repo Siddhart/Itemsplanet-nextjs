19376e0e1dc5faa9f714d68f38843f736b0dcc6a
Siddhart <Siddhart_7@live.nl>
2021-09-12 13:57:48 +0200

    google adsense

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,6 @@ export default class MyDocument extends Document {
           <script
             async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7427232256160357"
-            crossorigin="anonymous"
           ></script>
         </Head>
         <body>
